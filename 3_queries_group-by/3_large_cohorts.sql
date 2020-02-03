a62399da2ef6d8df5fdbbbea5981468a9f8ac17d
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-03 17:33:13 -0500

    get large cohorts

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,8 @@
+SELECT 
+  cohorts.name AS cohort_name,
+  count(students.*) AS student_count
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohort_name
+HAVING count(students.*) >= 18
+ORDER BY student_count;
