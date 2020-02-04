846dba3c6eb04406392c89cea12e66c3e1eb8a9d
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-04 14:40:25 -0500

    get avg assistance duration by cohorts

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,13 @@
+SELECT
+  avg(total_duration) as average_total_duration
+FROM (
+  SELECT
+    cohorts.name AS cohort,
+    SUM(completed_at - started_at) AS total_duration
+  FROM cohorts
+  JOIN students ON cohorts.id = cohort_id
+  JOIN assistance_requests ON students.id = student_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+  ) AS total_durations;
+
