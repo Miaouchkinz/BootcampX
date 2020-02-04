37c286c2c939ee3cfdc7f038e03e65c50935b4b7
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-04 14:30:18 -0500

    get total assistance requests by student

---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,7 @@
+SELECT 
+  count(assistance_requests.*) AS total_assistances,
+  students.name
+FROM students
+JOIN assistance_requests ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
